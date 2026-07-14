7ffc98b151d11f1e4baccd5e40fbe350b154ec4c
Patrick Baumgartner <contact@patbaumgartner.com>
2026-07-14 22:26:32 +0200

    chore(ci): consolidate dependabot automation and workflow hardening changes

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,8 +1,8 @@
 import js from '@eslint/js'
+import babelParser from '@babel/eslint-parser'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -11,12 +11,25 @@ export default defineConfig([
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
     languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: [
+            ['@babel/preset-typescript', { ignoreExtensions: true }],
+            '@babel/preset-react',
+          ],
+        },
+      },
       globals: globals.browser,
+    },
+    rules: {
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
     },
   },
 ])
